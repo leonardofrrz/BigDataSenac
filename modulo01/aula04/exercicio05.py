ff13b5023156e8e8df48aac94cc75eeca57b134f
LEONARDO FERRAZ <leonardo.ferraz@aluno.senac.rio>
2025-10-31 19:57:55 -0300

    arrumando exercicio0, adicionando notaFinal para deixar nota de backup

--- modulo01/aula04/exercicio05.py
+++ modulo01/aula04/exercicio05.py
@@ -15,12 +15,15 @@ nota1=float(input("Digite a nota da 1ª avaliação: "))
 nota2=float(input("Digite a nota da 2ª avaliação: "))
 nota_optativa=float(input("Digite a nota da avaliação optativa (-1 se não fez): "))
 
+notaFinal1= nota1
+notaFinal2= nota2
+
 if nota_optativa== -1:
-    media = (nota1 + nota2) / 2
+    media = (notaFinal1 + notaFinal2) / 2
     print(f"\nO aluno não realizou a prova optativa.")
 else:
-    nota_menor = min(nota1, nota2)
-    nota_maior = max(nota1, nota2)
+    nota_menor = min(notaFinal1, notaFinal2)
+    nota_maior = max(notaFinal1, notaFinal2)
     media = (nota_maior + nota_optativa) / 2 
     print(f"\nA nota optativa ({nota_optativa:.1f}) substituiu a menor nota ({nota_menor:.1f}).")
 
